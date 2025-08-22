de5e8f83e3f2eb7568e1867c9e5770827fb994e7
Aarav Ashwani <aaravashwani20@gmail.com>
2025-08-21 23:33:34 -0700

    movedistance method in chassis

--- src/lemlib/chassis/motions/moveToPoint.cpp
+++ src/lemlib/chassis/motions/moveToPoint.cpp
@@ -129,3 +129,120 @@ void lemlib::Chassis::moveToPoint(float x, float y, int timeout, MoveToPointPara
     distTraveled = -1;
     this->endMotion();
 }
+
+void lemlib::Chassis::moveDistance(float dist, int timeout, MoveToPointParams params, bool async) {
+    params.earlyExitRange = fabs(params.earlyExitRange);
+    this->requestMotionStart();
+    // were all motions cancelled?
+    if (!this->motionRunning) return;
+    // if the function is async, run it in a new task
+    if (async) {
+        pros::Task task([&]() { moveDistance(dist, timeout, params, false); });
+        this->endMotion();
+        pros::delay(10); // delay to give the task time to start
+        return;
+    }
+
+    // reset PIDs and exit conditions
+    lateralPID.reset();
+    lateralLargeExit.reset();
+    lateralSmallExit.reset();
+
+    // initialize vars used between iterations
+    Pose lastPose = getPose();
+    distTraveled = 0;
+    Timer timer(timeout);
+    bool close = false;
+    float prevLateralOut = 0; // previous lateral power
+    const int compState = pros::competition::get_status();
+    std::optional<bool> prevSide = std::nullopt;
+
+    Pose target(lastPose.x + dist * sin(lastPose.theta),
+                 lastPose.y + dist * cos(lastPose.theta));
+    target.theta = lastPose.angle(target);
+    printf("Target: %f, %f", target.x, target.y);
+
+    // main loop
+    while (!timer.isDone() && ((!lateralSmallExit.getExit() && !lateralLargeExit.getExit()) || !close) &&
+           this->motionRunning) {
+        // update position
+        const Pose pose = getPose(true, true);
+
+        // update distance traveled
+        distTraveled += pose.distance(lastPose);
+        lastPose = pose;
+
+        // calculate distance to the target point
+        const float distTarget = pose.distance(target);
+
+        // motion chaining
+        const bool side =
+            (pose.y - target.y) * -sin(target.theta) <= (pose.x - target.x) * cos(target.theta) + params.earlyExitRange;
+        if (prevSide == std::nullopt) prevSide = side;
+        const bool sameSide = side == prevSide;
+        // exit if close
+        if (!sameSide && params.minSpeed != 0) break;
+        prevSide = side;
+
+        // check if the robot is close enough to the target to start settling
+        if (distTarget < 7.5 && close == false) {
+            close = true;
+            params.maxSpeed = fmax(fabs(prevLateralOut), 60);
+        }
+
+        // calculate error
+        const float adjustedRobotTheta = params.forwards ? pose.theta : pose.theta + M_PI;
+        float lateralError = pose.distance(target) * cos(angleError(pose.theta, pose.angle(target)));
+
+
+        // update exit conditions
+        lateralSmallExit.update(lateralError);
+        lateralLargeExit.update(lateralError);
+
+        // get output from PIDs
+        float lateralOut = lateralPID.update(lateralError);
+
+        // apply restrictions on lateral speed
+        lateralOut = std::clamp(lateralOut, -params.maxSpeed, params.maxSpeed);
+        // constrain lateral output by max accel
+        // but not for decelerating, since that would interfere with settling
+        if (!close) lateralOut = slew(lateralOut, prevLateralOut, lateralSettings.slew);
+
+        // prevent moving in the wrong direction
+        if (params.forwards && !close) lateralOut = std::fmax(lateralOut, 0);
+        else if (!params.forwards && !close) lateralOut = std::fmin(lateralOut, 0);
+
+        // constrain lateral output by the minimum speed
+        if (params.forwards && lateralOut < fabs(params.minSpeed) && lateralOut > 0) lateralOut = fabs(params.minSpeed);
+        if (!params.forwards && -lateralOut < fabs(params.minSpeed) && lateralOut < 0)
+            lateralOut = -fabs(params.minSpeed);
+
+        // update previous output
+        prevLateralOut = lateralOut;
+
+        infoSink()->debug("Lateral Out: {}", lateralOut);
+
+        // ratio the speeds to respect the max speed
+        float leftPower = lateralOut;
+        float rightPower = lateralOut;
+        const float ratio = std::max(std::fabs(leftPower), std::fabs(rightPower)) / params.maxSpeed;
+        if (ratio > 1) {
+            leftPower /= ratio;
+            rightPower /= ratio;
+        }
+
+        // move the drivetrain
+        drivetrain.leftMotors->move(leftPower);
+        drivetrain.rightMotors->move(rightPower);
+        printf("\n %f", lateralOut);
+        // delay to save resources
+        pros::delay(10);
+    }
+
+    // stop the drivetrain
+    drivetrain.leftMotors->move(0);
+    drivetrain.rightMotors->move(0);
+    // set distTraveled to -1 to indicate that the function has finished
+    distTraveled = -1;
+    this->endMotion();
+}
